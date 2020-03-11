424dfcd9c1daeaf667cdfb75c452cfc8e8fe6dd8
Gary.Z <dinner3000@hotmail.com>
2020-03-11 14:39:46 +0800

    feat: change to postgresql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,8 +1,18 @@
-CREATE TABLE users (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  username varchar(100) NOT NULL,
-  first_name varchar(50) NOT NULL,
-  last_name varchar(50) DEFAULT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY UK_username (username)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE SEQUENCE public.users_id_seq
+    INCREMENT 1
+    START 1
+    CACHE 1;
+
+CREATE TABLE users
+(
+    id bigint NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    username character varying(100) NOT NULL,
+    first_name character varying(50) NOT NULL,
+    last_name character varying(50) DEFAULT NULL::character varying,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX users_username_uindex
+    ON users USING btree
+        (username);
